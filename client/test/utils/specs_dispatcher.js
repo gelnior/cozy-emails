9c5eddd8d653bb70afeaaaa6d002071ec951adf3
Frank Rousseau <frank.rousseau@cozycloud.cc>
2016-05-21 23:16:10 +0200

    [test] Add utils for tests
    * Fix dispatcher mock ;
    * Add utils to make mockery config easier.

--- client/test/utils/specs_dispatcher.js
+++ client/test/utils/specs_dispatcher.js
@@ -1,15 +1,15 @@
 var TestDispatcher = function TestDispatcher() {
-    this._callbacks = []
+    this._callbacks = [];
 }
 
 TestDispatcher.prototype.register = function (callback) {
-    this._callbacks.push(callback)
+    this._callbacks.push(callback);
 };
 
 
 TestDispatcher.prototype.dispatch = function (payload) {
     this._callbacks.forEach(function (callback) {
-        callback.call(this, {action: payload})
+        callback.call(this, payload);
     });
 };
 
